ef3df2b1fcdeb3c5b81bea54dd2344199df4640b
aihuidezo <aihuidezo@126.com>
2019-08-02 16:19:03 +0800

    commit cookie

--- src/main/java/com/lonely/zo/learnxd/controller/AuthorizeController.java
+++ src/main/java/com/lonely/zo/learnxd/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @Description
  * @Author ZB
@@ -29,7 +31,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")  String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -38,9 +41,13 @@ public class AuthorizeController {
         accessTokenDTO.setCode(code);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        System.out.println(user.getBio());
-        System.out.println(user.getId());
-        return "index";
+        if(user!=null){
+            //登录成功，写Cookie和session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else{
+            //登陆失败，重新登录
+            return "redirect:/";
+        }
     }
 }
