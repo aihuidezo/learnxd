71c427777f72b50ffe71e3c915d121f6b98d2361
aihuidezo <aihuidezo@126.com>
2019-08-08 15:14:06 +0800

    commit cookie

--- src/main/java/com/lonely/zo/learnxd/mapper/UserMapper.java
+++ src/main/java/com/lonely/zo/learnxd/mapper/UserMapper.java
@@ -3,9 +3,13 @@ package com.lonely.zo.learnxd.mapper;
 import com.lonely.zo.learnxd.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert({"insert into t_user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})"})
     void insert(User user);
+    @Select({"select * from t_user where token=#{token}"})
+    User findByToken(@Param("token") String token);
 }
